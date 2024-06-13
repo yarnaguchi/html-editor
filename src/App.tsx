89ad2ef32a3c89ba8a1fc97f96bbab1c1e84747c
山口絃 <gen.yamaguchi@exc.tokai-rika.co.jp>
2024-06-13 15:41:02 +0900

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, FC, useState } from "react";
+import CodeEditor from "@uiw/react-textarea-code-editor";
+import "./App.css";
 
-function App() {
+export const App: FC = () => {
+  const [code, setCode] = useState<string>(
+    "<p>abcde</p>\n<button>button</button>"
+  );
+
+  const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setCode(e.target.value);
+  };
+
+  // https://ja.react.dev/reference/react-dom/components/textarea
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* シンプルな <textarea> でのエディタ */}
+      <textarea value={code} onChange={onChange} rows={5} cols={33} />
+
+      {/*
+      これは遊びでやってみただけ。
+      これを使うと npm run start で警告が出てるのが気になる。
+      他のライブラリを使うなら「react syntax highlighter」でググるといいかも。
+      */}
+      <CodeEditor
+        className="code-editor"
+        value={code}
+        language="html"
+        onChange={onChange}
+        padding={10}
+      />
+
+      {/* <iframe> はうまく内部のhtmlを更新できなかったのでやめた */}
+      <div className="preview" dangerouslySetInnerHTML={{ __html: code }} />
     </div>
   );
-}
+};
 
 export default App;
